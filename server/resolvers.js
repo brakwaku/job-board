e1823ffd76a87b4187390536ade4aa87f360dd06
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2023-02-12 19:25:14 -0700

    A lot has happened

---
+++ server/resolvers.js
@@ -0,0 +1,28 @@
+import { Job, Company } from './db.js';
+
+export const resolvers = {
+  Query: {
+    company: (_root, { id }) => Company.findById(id),
+    job: (_root, { id }) => Job.findById(id),
+    jobs: () => Job.findAll(),
+  },
+
+  Mutation: {
+    createJob: (_root, { input }, { user }) => { // user is destructured from the context object set in server.js
+      if (!user) {
+        throw new Error('Unauthorized');
+      }
+      return Job.create({ ...input, companyId: user.companyId });
+    },
+    deleteJob: (_root, { id }) => Job.delete(id),
+    updateJob: (_root, { input }) => Job.update(input),
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
+  },
+};
